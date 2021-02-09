ffb16096e472a14efc9e6a14d1b58810bc2f2108
Vladimir Grigoryev <volodimir.grigoryev@gmail.com>
2021-02-09 16:17:42 +0000

    refuse using ORM

--- src/database/db_api.py
+++ src/database/db_api.py
@@ -1,20 +1,36 @@
-from database.models import CallTable, get_cursor
+import sqlite3
+from datetime import datetime
 
 
 class DbApi:
+    connect = None
+
+    @staticmethod
+    def connect(db_path):
+        print(db_path)
+        DbApi.connect = sqlite3.connect(db_path)
+
+    @staticmethod
+    def get_cursor():
+        return DbApi.connect.cursor()
+
+    @staticmethod
+    def close():
+        DbApi.connect.close()
+
     @staticmethod
     def add_new_call(_date, _phone_number, _talk_time, _call_status):
-        CallTable.create(
-            date=_date,
-            phone_number=_phone_number,
-            talk_time=_talk_time,
-            call_status=_call_status
-        )
+        query = f"""
+                insert into Call_table(date, phone_number, talk_time, call_status)
+                values ({_date}, {_phone_number}, {_talk_time}, {_call_status})
+        """
+
+        DbApi.get_cursor().execute(query)
 
     @staticmethod
     def get_calls_info_by_date(_date_start, _date_end):
         """_date_* - datetime object"""
-        cursor = get_cursor()
+        cursor = DbApi.get_cursor()
         date_start = _date_start.strftime('%Y-%m-%d')
         date_end = _date_end.strftime('%Y-%m-%d')
         # strftime - первый день недели - воскресенье
@@ -22,7 +38,7 @@ class DbApi:
                     select strftime('%w', date) WeekNumber,
                     strftime('%H', date) HourNumber,
                     count(id)/(
-                        SELECT DISTINCT
+                        select DISTINCT
                         cast((strftime('%d', '{date_end}') - strftime('%d', '{date_start}') + 1.0)/7.0 as int) * 1.
                         from Call_table
                     )
@@ -36,4 +52,12 @@ class DbApi:
 
     @staticmethod
     def get_first_date():
-        return CallTable.select().limit(1).order_by(CallTable.id).get().date
+        cursor = DbApi.get_cursor()
+        query = f"""
+            select date
+            from Call_table
+            order by id limit 1
+        """
+
+        cursor.execute(query)
+        return datetime.strptime(cursor.fetchall()[0][0], '%Y-%m-%d %H:%M:%S')
